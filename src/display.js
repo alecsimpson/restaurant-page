35402218380383f96b38b42ddafaad6ff0313d6c
Alec Simpson <alecsimpson20@gmail.com>
2023-02-01 14:38:20 -0800

    added displayHeader and displayHome methods for displayController

--- src/display.js
+++ src/display.js
@@ -1,39 +1,68 @@
 import './style.css';
+import foodPic from '../assets/Bangers-and-Mash-2.png';
 
 const displayController = (() => {
+  const wrapperEl = document.createElement('div');
+  wrapperEl.id = 'wrapper';
+
+  const contentEl = document.createElement('div');
+  contentEl.id = 'content';
+
+  wrapperEl.appendChild(contentEl);
+  document.querySelector('body').appendChild(wrapperEl);
+
+  const createEl = (type, style, inner) => {
+    const el = document.createElement(type);
+    if (style) {
+      el.classList.add(style);
+    }
+    el.innerHTML = inner;
+
+    return el;
+  };
+
   const displayHeader = () => {
     const headerEl = document.createElement('div');
     headerEl.id = 'header';
 
-    const titleEl = document.createElement('div');
-    titleEl.classList.add('title');
-    titleEl.innerHTML = 'The Irish Pub';
+    const titleEl = createEl('div', 'title', 'The Irish Pub');
     headerEl.appendChild(titleEl);
 
-    const tabsEl = document.createElement('div');
-    tabsEl.classList.add('tabs');
+    const tabsEl = createEl('div', 'tabs', '');
 
-    const homeTabEl = document.createElement('div');
-    homeTabEl.classList.add('home');
-    homeTabEl.innerHTML = 'Home';
+    const homeTabEl = createEl('div', 'home', 'Home');
     tabsEl.appendChild(homeTabEl);
 
-    const menuTabEl = document.createElement('div');
-    menuTabEl.classList.add('menu');
-    menuTabEl.innerHTML = 'Menu';
+    const menuTabEl = createEl('div', 'menu', 'Menu');
     tabsEl.appendChild(menuTabEl);
 
-    const contactTabEl = document.createElement('div');
-    contactTabEl.classList.add('contact');
-    contactTabEl.innerHTML = 'Contact';
+    const contactTabEl = createEl('div', 'contact', 'Contact');
     tabsEl.appendChild(contactTabEl);
 
     headerEl.appendChild(tabsEl);
-    document.querySelector('body').appendChild(headerEl);
+    wrapperEl.prepend(headerEl);
+  };
+
+  const displayHome = () => {
+    const introEl = createEl('div', 'intro', 'The best place off the island for beer, bangers and bollocks!');
+
+    const slideshowEl = createEl('div', 'slideshow', '');
+    const imgEl = createEl('img', '', '');
+    imgEl.src = foodPic;
+    slideshowEl.appendChild(imgEl);
+
+    const addrLine1El = createEl('div', 'address-line-1', '1234 Taraval Street');
+    const addrLine2El = createEl('div', 'address-line-2', 'San Francisco CA, 94122');
+
+    contentEl.appendChild(introEl);
+    contentEl.appendChild(slideshowEl);
+    contentEl.appendChild(addrLine1El);
+    contentEl.appendChild(addrLine2El);
   };
 
   return {
     displayHeader,
+    displayHome,
   };
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import displayController from './display';
 
 displayController.displayHeader();
+displayController.displayHome();
